84c50e751fe1e0e1cf7245183dbdc12fe8c6461d
mmorrow6109 <msmorrow6109@gmail.com>
2023-11-15 16:38:50 -0800

    variables and logic implemented.

--- Develop/script.js
+++ Develop/script.js
@@ -5,12 +5,47 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to be?");
 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters");
+    return;
+  }
+
+   else {
+    var lowercaseConfirm = confirm("Would you like to include lowercase letters?");
+    var uppercaseConfirm = confirm("Would you like to include uppercase letters?");
+    var numbersConfirm = confirm("Would you like to include numbers?");
+    var specialCharactersConfirm = confirm("Would you like to include special characters?");
+    var optionsString = "";
+    
+    if (lowercaseConfirm) {
+     optionsString += lowercase;
+    }
+    
+    if (uppercaseConfirm) {
+      optionsString += uppercase;
+    }
+
+    if (numbersConfirm) {
+      optionsString += numbers;
+    }
+
+    if (specialCharactersConfirm) {
+      optionsString += specialCharacters;
+    }
+    
+   }
+
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
   var password = generatePassword();
+  
 
 
   
@@ -21,4 +56,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generateBtn);
+generateBtn.addEventListener("click", writePassword);
